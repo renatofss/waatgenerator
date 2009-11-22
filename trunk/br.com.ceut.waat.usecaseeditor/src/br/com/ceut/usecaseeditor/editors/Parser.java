bc142a299ce7aaddf676b384eeb5e3d0247c17b4
cid.ernesto <cid.ernesto@b6f1014e-d7be-11de-a675-df08bf6041cf>
2009-11-22 23:52:33 +0000

    Initial commit

---
+++ trunk/br.com.ceut.waat.usecaseeditor/src/br/com/ceut/usecaseeditor/editors/Parser.java
@@ -0,0 +1,28 @@
+package br.com.ceut.usecaseeditor.editors;
+
+public interface Parser {
+	String[] KEYWORDS = { "Cenario", 
+							"Pre-Condicao",
+							"Executar",
+							"Ha",
+							"caminhos",
+							"valida",
+							"Link",
+							"sessao",
+							"Campo",
+							"Select",
+							"Botao",
+							"Formulario",
+							"Atributos",
+							"Mensagem",
+							"clica",
+							"seleciona",
+							"preenche",
+							"inclui",
+							"apaga",
+							"exibe",
+							"valida",
+							"Usuario",
+							"Sistema"
+						};
+}
