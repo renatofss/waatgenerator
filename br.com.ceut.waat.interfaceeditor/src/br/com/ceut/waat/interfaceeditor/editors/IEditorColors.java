c6a3ac1c2c124e7d545362c01cec8a5895b60ef3
cid.ernesto <cid.ernesto@b6f1014e-d7be-11de-a675-df08bf6041cf>
2009-11-22 23:44:48 +0000

    Initial commit

---
+++ br.com.ceut.waat.interfaceeditor/src/br/com/ceut/waat/interfaceeditor/editors/IEditorColors.java
@@ -0,0 +1,8 @@
+package br.com.ceut.waat.interfaceeditor.editors;
+
+import org.eclipse.swt.graphics.RGB;
+
+public interface IEditorColors {
+	RGB KEYWORD = new RGB(0, 41, 170);
+	RGB STRING = new RGB(51, 115, 0);
+}
